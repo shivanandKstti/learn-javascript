6f5fa79880c398d5717b62c39934fff9fc525231
shivusoumya <shivadatacamp@gmail.com>
2022-05-15 21:52:49 +0530

    --- Auto-Git Commit ---

--- moshCourse/js/app.js
+++ moshCourse/js/app.js
@@ -1,5 +1,15 @@
 // console.log('Hello world');
 
-let interestRate = 0.3;
+// let interestRate = 0.3;
 
-console.log(interestRate);
+// console.log(interestRate);
+
+
+/**
+ * primitive data types0
+ */
+let name = "shiva"; // string literal
+let age = 30; // number literal
+let isApproved = true; // boolean literal
+let firstName = undefined;
+let lastName = null;
