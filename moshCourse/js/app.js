71f9dbcc5185adfd607477b31c44289a146a4af8
shivusoumya <shivadatacamp@gmail.com>
2022-05-15 22:23:23 +0530

    --- Auto-Git Commit ---

--- moshCourse/js/app.js
+++ moshCourse/js/app.js
@@ -6,10 +6,63 @@
 
 
 /**
- * primitive data types0
+ * primitive data types
  */
 let name = "shiva"; // string literal
 let age = 30; // number literal
 let isApproved = true; // boolean literal
 let firstName = undefined;
 let lastName = null;
+
+/**
+ * dynamic type
+ */
+
+
+/**
+ * Reference type
+ * object
+ * arrays
+ * 
+ */
+
+let person = {
+    name: 'shiva',
+    age: 29
+}
+
+// console.log(person);
+// console.log(person.name);
+
+let selectedColors = ['red', 'blue'];
+console.log(selectedColors[0]);
+selectedColors[2] = 1;
+console.log(selectedColors.length);
+let persons = [{
+        name: 'shiva',
+        age: 29
+    },
+    {
+        name: 'savi',
+        age: 22
+    }
+]
+
+// console.log(persons);
+
+/**
+ * functions
+ */
+
+function greet(name = 'unknown', lastName = 'lastName') {
+    console.log(`hello ${name} ${lastName}`);
+}
+
+function square(number) {
+    return number * number;
+}
+
+console.log(square(2));
+
+// greet();
+// greet('shiva', 'katti');
